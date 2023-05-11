5ec5ef251cdff93ee747cb913318b6b72591d0d6
Arina <arina.milyutina.0005@gmail.com>
2023-05-11 16:24:05 +0300

    add field id, add annotations

--- src/main/java/by/tms/Operation.java
+++ src/main/java/by/tms/Operation.java
@@ -1,12 +1,31 @@
 package by.tms;
 
-import lombok.Data;
+import lombok.*;
 
-@Data
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "operation")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Operation {
     private double num1;
     private double num2;
     private double result;
     private String type;
+    private long id;
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    public Long getId() {
+        return id;
+    }
 }
