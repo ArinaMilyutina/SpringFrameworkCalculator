18fdb7f9400fdc5722344a64b3bc3b5ee4c23f4d
Arina <arina.milyutina.0005@gmail.com>
2023-05-11 15:41:40 +0300

    correct

--- src/main/java/by/tms/HibernateUserDao.java
+++ src/main/java/by/tms/HibernateUserDao.java
@@ -4,10 +4,12 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 //import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 //import java.util.List;
-
+@Service
 public class HibernateUserDao {
     @Autowired
     private SessionFactory sessionFactory;
