70e967cffc11260f8497cc9b9f3bb983f1aeacdb
Arina <arina.milyutina.0005@gmail.com>
2023-05-11 15:31:46 +0300

    add field id, add annotations

--- src/main/java/by/tms/User.java
+++ src/main/java/by/tms/User.java
@@ -1,10 +1,30 @@
 package by.tms;
 
-import lombok.Data;
+import lombok.*;
 
-@Data
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
     private String name;
     private String userName;
     private String password;
+    private long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    public Long getId() {
+        return id;
+    }
 }
