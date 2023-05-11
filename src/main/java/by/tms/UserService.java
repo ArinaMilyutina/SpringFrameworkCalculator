91567736e093673808a0bc75c51505d8b52fe32b
Arina <arina.milyutina.0005@gmail.com>
2023-05-11 16:04:39 +0300

    add method findByUserName

--- src/main/java/by/tms/UserService.java
+++ src/main/java/by/tms/UserService.java
@@ -3,6 +3,8 @@ package by.tms;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class UserService {
     @Autowired
@@ -10,6 +12,10 @@ public class UserService {
 
     public void createUser(User user) {
         hibernateUserDao.save(user);
+    }
+
+    public Optional<User> findByUsername(String username) {
+        return hibernateUserDao.findByUsername(username);
     }
 
 }
