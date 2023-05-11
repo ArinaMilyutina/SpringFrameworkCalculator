16dd1bef8c8516922b811d3993dab3c164791e21
Arina <arina.milyutina.0005@gmail.com>
2023-05-11 16:23:43 +0300

    add method createOperation

--- src/main/java/by/tms/OperationService.java
+++ src/main/java/by/tms/OperationService.java
@@ -8,31 +8,40 @@ import org.springframework.stereotype.Service;
 public class OperationService {
     @Autowired
     private Calculator calculator;
+    @Autowired
+    private HibernateOperationDao hibernateOperationDao;
+    private static String MESSAGE_ERROR = "check the correctness of the entered data!!!";
 
     public Operation calculate(Operation operation) {
 
 
         if (operation.getType().equals("sum")) {
             operation.setResult(calculator.sum(operation.getNum1(), operation.getNum2()));
+            createOperation(operation);
             return operation;
         }
         if (operation.getType().equals("div") && operation.getNum2() != 0) {
             operation.setResult(calculator.div(operation.getNum1(), operation.getNum2()));
-
+            createOperation(operation);
             return operation;
         }
         if (operation.getType().equals("mul")) {
             operation.setResult(calculator.mul(operation.getNum1(), operation.getNum2()));
-
+            createOperation(operation);
             return operation;
         }
         if (operation.getType().equals("res")) {
             operation.setResult(calculator.res(operation.getNum1(), operation.getNum2()));
-
+            createOperation(operation);
             return operation;
         } else {
-            throw new RuntimeException("check the correctness of the entered data!!!");
+            throw new RuntimeException(MESSAGE_ERROR);
         }
+
+    }
+
+    public void createOperation(Operation operation) {
+        hibernateOperationDao.save(operation);
 
     }
 
