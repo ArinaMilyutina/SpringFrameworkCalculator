9cbc296b42398eae090e5959f64ccecff3fa0b93
Arina <arina.milyutina.0005@gmail.com>
2023-05-11 16:05:22 +0300

    add Optional

--- src/main/java/by/tms/HibernateUserDao.java
+++ src/main/java/by/tms/HibernateUserDao.java
@@ -2,11 +2,14 @@ package by.tms;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-//import org.hibernate.query.Query;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
 
 //import java.util.List;
 @Service
@@ -25,17 +28,17 @@ public class HibernateUserDao {
 //        return currentSession.get(User.class, id);
 //    }
 
-//    public List<User> findAll() {
+    //    public List<User> findAll() {
 //        Session currentSession = sessionFactory.getCurrentSession();
 //        List<User> fromUser = currentSession.createQuery("from User", User.class).getResultList();
 //        return fromUser;
 //    }
 //
-//    public User findByUsername(String username) {
-//        Session currentSession = sessionFactory.getCurrentSession();
-//        Query<User> query = currentSession.createQuery("from User where username = :username", User.class);
-//        query.setParameter("username", username);
-//        User singleResult = query.getSingleResult();
-//        return singleResult;
-//    }
+    public Optional<User> findByUsername(String username) {
+        Session currentSession = sessionFactory.getCurrentSession();
+        Query<User> query = currentSession.createQuery("from User where username = :username", User.class);
+        query.setParameter("username", username);
+        User singleResult = query.getSingleResult();
+        return Optional.of(singleResult);
+    }
 }
