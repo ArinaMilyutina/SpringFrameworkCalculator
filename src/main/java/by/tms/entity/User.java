2c006b13100aa32170d0700a7db7a1d4c781407a
Arina <arina.milyutina.0005@gmail.com>
2023-05-11 16:43:58 +0300

    add package entity

--- src/main/java/by/tms/entity/User.java
+++ src/main/java/by/tms/entity/User.java
@@ -1,4 +1,30 @@
 package by.tms.entity;
 
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "us")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    private String name;
+    private String username;
+    private String password;
+    private long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    public Long getId() {
+        return id;
+    }
 }
