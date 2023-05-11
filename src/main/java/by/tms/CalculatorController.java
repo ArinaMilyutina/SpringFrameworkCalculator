2c6f88ee378cc3b5e4b0c2e05337adcebeda510c
Arina <arina.milyutina.0005@gmail.com>
2023-05-11 15:12:25 +0300

    create class CalculatorController, add method calc

---
+++ src/main/java/by/tms/CalculatorController.java
@@ -0,0 +1,31 @@
+package by.tms;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/calc")
+public class CalculatorController {
+    @Autowired
+    private OperationService operationService;
+
+    @GetMapping
+    public String calc() {
+        return "calc";
+    }
+
+    @PostMapping
+    public String calc(Operation operations, Model model) {
+        Operation result = operationService.calculate(operations);
+        model.addAttribute("result", result);
+        return "calc";
+    }
+
+
+}
