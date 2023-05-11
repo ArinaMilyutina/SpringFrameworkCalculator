3caf1bd94489564373d17ffe90c736746a9610e2
Arina <arina.milyutina.0005@gmail.com>
2023-05-11 16:04:58 +0300

    correct

--- src/main/java/by/tms/UserController.java
+++ src/main/java/by/tms/UserController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Optional;
+
 @Controller
 @RequestMapping("/user")
 public class UserController {
@@ -19,9 +21,17 @@ public class UserController {
 
     @GetMapping("/authorization")
     public String auth() {
-        return "redirect:/calc";
+        return "auth";
     }
 
+    @PostMapping("/authorization")
+    public String auth(String username) {
+        Optional<User> byUsername = userService.findByUsername(username);
+        if (byUsername.isPresent()) {
+            return "redirect:/calc";
+        }
+        return "User not found!!!";
+    }
 
     @PostMapping("/registration")
     public String reg(User user) {
